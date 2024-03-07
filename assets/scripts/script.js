179eb795502b1685f39eb837ba013c64c0a72cff
Hussein Jaber <110312844+HOSSJBR@users.noreply.github.com>
2024-03-06 21:58:22 -0500

    Add files via upload

---
+++ assets/scripts/script.js
@@ -0,0 +1,29 @@
+const instructionButtons = document.querySelector("#instructions-button");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const allBtnSound = document.querySelector(".allbtnsound");
+const enterButton = document.querySelector("#enter-button");
+
+// modal instructions //
+function instructionList() {
+  allBtnSound.play();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+} // Close button
+function closeInstruction() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+} // Escape button function
+function closingEscape(e) {
+  if (e.key === "Escape") {
+    closeInstruction();
+    hideLeaderBoard();
+  }
+}
+
+const close = document.querySelector(".close");
+close.addEventListener("click", closeInstruction);
+instructionButtons.addEventListener("click", instructionList);
+enterButton.addEventListener("click", () => {
+  allBtnSound.play();
+});
